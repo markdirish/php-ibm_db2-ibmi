9d17660ac2dfb6e475fbcc79ec6b2262797847a4
Jesse Gorzinski <jgorzins@us.ibm.com>
2020-08-20 10:11:35 -0500

    bump version

--- php_ibm_db2.h
+++ php_ibm_db2.h
@@ -23,7 +23,7 @@
   $Id$
 */
 
-#define	PHP_IBM_DB2_VERSION	"2.0.8-zs3"
+#define	PHP_IBM_DB2_VERSION	"3.0.0i" // trailing 'i' to indicate IBM i flavor of this module
 
 #ifndef PHP_IBM_DB2_H
 #define PHP_IBM_DB2_H
